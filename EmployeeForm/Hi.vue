3719894cab7c8e9be45c80e2d5645cb734553847
Evan Gonshor <EvanGonshor@engineer.com>
2022-04-01 18:29:57 -0400

    Vue Intro

---
+++ EmployeeForm/Hi.vue
@@ -0,0 +1,32 @@
+<template lang="jade">
+div
+  p {{ greeting }} World!
+  OtherComponent
+  <input type="radio" id="one" value="One" v-model="picked">
+<label for="one">One</label>
+<br>
+<input type="radio" id="two" value="Two" v-model="picked">
+<label for="two">Two</label>
+<br>
+<span>Picked: {{ picked }}</span>
+</template>
+
+<script>
+import OtherComponent from "./OtherComponent.vue";
+export default {
+  components: {
+    OtherComponent,
+  },
+  data() {
+    return {
+      greeting: "Hello",
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+p
+  font-size 2em
+  text-align center
+</style>
---
+++ EmployeeForm/my-app/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <p>Is New Employee?</p>
+    <input type="radio" id="yes" value="Yes" v-model="picked" />
+    <label for="yes">Yes</label>
+
+    <input type="radio" id="no" value="No" v-model="picked" />
+    <label for="no">No</label>
+    <br />
+    <input v-model="message" placeholder="edit me" />
+    <br />
+    <label style="padding-right: 20px" for="selected">Gender</label>
+    <select v-model="selected">
+      <option>Male</option>
+      <option>Female</option>
+      <option>Other</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      selected: "",
+      newemployee: [],
+      message: "",
+      picked: "Yes",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 5px;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
